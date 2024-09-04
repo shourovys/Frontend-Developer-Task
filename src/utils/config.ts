663c0e51b8732126c64e47557d3e052fd6852db1
Shourov Saha <shourovys@gmail.com>
2024-09-05 04:50:37 +0600

    build: api url update

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,4 +1,6 @@
 export const TABLE_ROW_HEIGHT = 92;
 export const TABLE_ROW_PER_PAGE = 10;
 
-export const API_URL = 'http://localhost:3000/api';
+export const API_URL =
+  'https://fully-functional-order-table-by-shourov-saha.vercel.app/api';
+// export const API_URL = 'http://localhost:3000/api';
