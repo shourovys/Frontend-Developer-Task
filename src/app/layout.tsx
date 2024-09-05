3cbd2734ae00ccc8542458e62026f8aa042890e1
Shourov Saha <shourovys@gmail.com>
2024-09-05 07:39:15 +0600

    style: container padding update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
     <html lang='en'>
       <body className={inter.className}>
         <Navbar />
-        <main className='container px-4 md:px-6 2xl:px-0 mx-auto'>
+        <main className='container px-4 md:px-3 2xl:px-0 mx-auto'>
           {children}
         </main>
       </body>
